fcdc9ea2ad7fcecd5858f55bbcf5f5159c9abd4a
Tomasz Grande <grande.tomasz@gmail.com>
2020-05-04 17:02:37 +0200

    #7 rewrite used styles to styled component version

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,8 +8,15 @@
 import React, { FunctionComponent } from "react";
 import { useStaticQuery, graphql } from "gatsby";
 
+import styled from "styled-components";
 import Header from "./header";
 import "./layout.css";
+
+const Div = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45rem;
+`;
 
 const Layout: FunctionComponent = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,20 +32,14 @@ const Layout: FunctionComponent = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <Div>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </div>
+      </Div>
     </>
   );
 };
