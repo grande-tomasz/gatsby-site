c6e02fb342da69e26027206388461a8e948ffa69
Tomasz Grande <grande.tomasz@gmail.com>
2020-05-06 22:21:37 +0200

    #6 apply required adjustments to existing rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,46 +5,62 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:prettier/recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/typescript",
-    "airbnb",
-    "prettier",
-    "prettier/react",
-    "prettier/@typescript-eslint",
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'prettier/react',
+    'prettier/@typescript-eslint',
   ],
   globals: {
     __PATH_PREFIX__: true,
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
-    sourceType: "module",
+    ecmaVersion: 2020,
+    sourceType: 'module',
   },
-  plugins: ["prettier", "react", "@typescript-eslint"],
-  rules: {},
+  plugins: ['@typescript-eslint', 'prettier', 'react'],
+  rules: {
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
   overrides: [
     {
-      files: ["*.tsx"],
+      files: ['*.tsx'],
       rules: {
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "import/extensions": "off",
-        "import/no-unresolved": "off",
-        "react/jsx-filename-extension": "off",
-        "react/prop-types": "off",
+        'react/prop-types': 'warn',
       },
     },
     {
-      files: "gatsby-config.js",
+      files: 'gatsby-config.js',
       rules: {
-        "@typescript-eslint/camelcase": "off",
+        '@typescript-eslint/camelcase': 'off',
       },
+    },
+    {
+      files: ['*.test.js', '*.test.ts'],
+      env: { jest: true },
     },
   ],
 };
