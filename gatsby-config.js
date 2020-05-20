f0c16a3fb227f027caa5919183d1b71e24d561df
Tomasz Grande <grande.tomasz@gmail.com>
2020-05-20 20:06:00 +0200

    #3 add link site metadata for testing purposes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     description:
       'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
     author: '@gatsbyjs',
+    link: 'http://mysite.com',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
