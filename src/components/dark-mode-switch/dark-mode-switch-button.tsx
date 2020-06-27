8f31f6e42e251d682c492aebb45f6d986cd25a03
Tomasz Grande <grande.tomasz@gmail.com>
2020-06-27 11:21:06 +0200

    #32 add styles for dark mode switch button

---
+++ src/components/dark-mode-switch/dark-mode-switch-button.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import { darkModeSwitchColor } from '../../constants/colors';
+import Button from '../styled/button';
+
+interface IDarkModeSwitchButtonProps {
+  darkMode: boolean;
+}
+
+const DarkModeSwitchButton = styled(Button)<IDarkModeSwitchButtonProps>`
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: min(10vw, 60px);
+
+  svg {
+    fill: ${(props) => (props.darkMode ? darkModeSwitchColor.dark : darkModeSwitchColor.light)};
+    transition-property: fill, filter;
+    transition-duration: 0.3s;
+  }
+
+  #dark-mode-switch_moon {
+    transform: ${(props) => (props.darkMode ? 'translateX(90px)' : 'translateX(0)')};
+    transition-property: transform;
+    transition-duration: 0.3s;
+  }
+
+  &:focus,
+  &:hover {
+    svg {
+      fill: ${(props) => (props.darkMode
+          ? darkModeSwitchColor.hover.dark
+          : darkModeSwitchColor.hover.light)};
+
+      filter: drop-shadow(1px 1px 1px rgba(255, 255, 255, 0.1))
+        drop-shadow(1px -1px 1px rgba(255, 255, 255, 0.1))
+        drop-shadow(-1px 1px 1px rgba(255, 255, 255, 0.1))
+        drop-shadow(-1px -1px 1px rgba(255, 255, 255, 0.1));
+    }
+  }
+`;
+
+export default DarkModeSwitchButton;
