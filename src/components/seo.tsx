cbb17df961e363a5106327c74a9a3f979fabd63d
Tomasz Grande <grande.tomasz@gmail.com>
2020-07-03 23:41:35 +0200

    #37 add basic implementation of seo component

---
+++ src/components/seo.tsx
@@ -0,0 +1,119 @@
+import { graphql, useStaticQuery } from 'gatsby';
+import React, { FunctionComponent } from 'react';
+import { Helmet, HelmetProps } from 'react-helmet';
+
+type SEOProps = {
+  description?: string;
+  lang?: string;
+  meta?: HelmetProps['meta'];
+  link?: HelmetProps['link'];
+  title: HelmetProps['title'];
+};
+
+const SEO: FunctionComponent<SEOProps> = ({
+  description = '',
+  lang = 'en',
+  meta = [],
+  link = [],
+  title,
+}) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+            url
+            image
+          }
+        }
+      }
+    `,
+  );
+
+  const metaDescription = description || site.siteMetadata.description;
+
+  const standardMetaTags: HelmetProps['meta'] = [
+    {
+      name: 'description',
+      content: metaDescription,
+    },
+  ];
+
+  const openGraphMetaTags: HelmetProps['meta'] = [
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      property: 'og:title',
+      content: title,
+    },
+    {
+      property: 'og:description',
+      content: metaDescription,
+    },
+    {
+      property: 'og:image',
+      content: `${site.siteMetadata.url}${site.siteMetadata.image}`,
+    },
+    {
+      property: 'og:url',
+      content: site.siteMetadata.url,
+    },
+  ];
+
+  const twitterMetaTags: HelmetProps['meta'] = [
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+    {
+      name: 'twitter:site',
+      content: site.siteMetadata.url,
+    },
+    {
+      name: 'twitter:creator',
+      content: site.siteMetadata.author,
+    },
+    {
+      name: 'twitter:title',
+      content: title,
+    },
+    {
+      name: 'twitter:description',
+      content: metaDescription,
+    },
+    {
+      name: 'twitter:image',
+      content: `${site.siteMetadata.url}${site.siteMetadata.image}`,
+    },
+  ];
+
+  return (
+    <Helmet
+      // base
+      htmlAttributes={{
+        lang,
+      }}
+      // bodyAttributes
+      defaultTitle={`${site.siteMetadata.title}`}
+      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      title={title}
+      meta={[
+        ...standardMetaTags,
+        ...openGraphMetaTags,
+        ...twitterMetaTags,
+        ...meta,
+      ]}
+      link={link}
+      // style
+      // script
+      // noscript
+    />
+  );
+};
+
+export default SEO;
