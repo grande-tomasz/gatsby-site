73db96a9aca63b0473ba9496e688f2384562a82a
Tomasz Grande <grande.tomasz@gmail.com>
2020-08-31 03:49:29 +0200

    #40 add social icons constants

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,20 +1,33 @@
-export const lightBackgroundColor = 'Gainsboro';
-export const darkBackgroundColor = 'MidnightBlue';
+export const lightBackgroundColor = '#F2F2F2';
+export const darkBackgroundColor = '#001020';
 export const lightTextColor = 'White';
 export const darkTextColor = 'Black';
+
+const defaultLight = 'Gray';
+const defaultDark = 'Silver';
 
 export const logoColor = {
   T: '#000080',
   G: '#4B0082',
-  dark: 'Silver',
+  dark: defaultDark,
 };
 
 export const darkModeSwitchColor = {
-  light: 'Gray',
-  dark: 'Silver',
+  light: defaultLight,
+  dark: defaultDark,
 
   hover: {
     light: '#FF9800',
     dark: '#FFC107',
   },
+};
+
+export const socialIconColor = {
+  light: defaultLight,
+  dark: defaultDark,
+  linkedIn: '#0077B5',
+  gitHub: '#181717',
+  gmail: '#D14836',
+  skype: '#00AFF0',
+  phoneCall: '#333333',
 };
